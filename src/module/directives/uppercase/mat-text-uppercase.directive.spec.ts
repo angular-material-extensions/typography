a2a66ff1137bcd90ed29d8c675afccae86a6c904
Anthony Nahas <anthony.na@hotmail.de>
2018-06-04 23:59:51 +0200

    chore(package): added MatTextUppercase Directive

---
+++ src/module/directives/uppercase/mat-text-uppercase.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MatTextUppercaseDirective } from './mat-text-uppercase.directive';
+
+describe('MatTextUppercaseDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MatTextUppercaseDirective();
+    expect(directive).toBeTruthy();
+  });
+});
